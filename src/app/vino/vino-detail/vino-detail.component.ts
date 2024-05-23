799e920e6ca4bd909f13bf86642915d7c828c0b5
Gabriel <gabriel.lamela.ch@gmail.com>
2024-05-23 11:44:09 +0200

    Hacer request de vino en vino-detail

--- src/app/vino/vino-detail/vino-detail.component.ts
+++ src/app/vino/vino-detail/vino-detail.component.ts
@@ -3,6 +3,8 @@ import { ActivatedRoute, Params, Router } from '@angular/router';
 
 import { Vino } from '../vino.model';
 import { VinoService } from '../vino.service';
+import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-vino-detail',
@@ -17,14 +19,50 @@ export class VinoDetailComponent implements OnInit {
   constructor(
     private vinoService: VinoService,
     private route: ActivatedRoute,
-    private router: Router
+    private router: Router,
+    private http: HttpClient
   ) { }
 
   ngOnInit(): void {
     this.route.params.subscribe(
       (params: Params) => {
         this.id = +params['id'];
-        this.vino = this.vinoService.getVino(this.id)!;
+        this.http.get<{
+          'Id' : number,
+          'Nombre' : string,
+          'Precio' : number,
+          'Tipo' : string,
+          'Region' : string,
+          'Bodega' : string,
+          'Anada' : number,
+          'Alergenos' : string,
+          'Graduacion' : number,
+          'BreveDescripcion' : string,
+          'Capacidad' : number,
+          'Stock' : number | null
+        }[]>(
+          `${environment.apiUrl}vinos/${this.id}`
+        )
+          .subscribe({
+            next: (response) => {
+              this.vino = new Vino(
+                response[0]['Id'],
+                response[0]['Nombre'],
+                response[0]['Precio'],
+                response[0]['Region'],
+                response[0]['Tipo'],
+                response[0]['Bodega'],
+                response[0]['Anada'],
+                response[0]['Alergenos'],
+                response[0]['Graduacion'],
+                response[0]['BreveDescripcion'],
+                response[0]['Capacidad'],
+                response[0]['Stock'],
+                '',
+                null
+              );
+            }
+        })
       }
     );
   }
